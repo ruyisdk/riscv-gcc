5d017d5a033a71e8b994e354c66d4359fa1959d8
Jin Ma <jinma@linux.alibaba.com>
2023-01-09 14:17:34 +0800

    [T-HEAD][RISCV] Fixup: leaf function should not generate ipush/ipop insns.

--- gcc/testsuite/gcc.target/riscv/thead/ipush-not.c
+++ gcc/testsuite/gcc.target/riscv/thead/ipush-not.c
@@ -2,14 +2,11 @@
 /* { dg-skip-if "test ipush/ipop insns" { *-*-* } { "*" } { "-march=*xtheade*" } } */
 /* { dg-options "--save-temps -O2" } */
 
-extern void f(void);
-
 __attribute__((interrupt)) void func(void)
 {
-  f ();
 }
 
-/* { dg-final { scan-assembler "ipush" } }*/
-/* { dg-final { scan-assembler "ipop" } }*
+/* { dg-final { scan-assembler-not "ipush" } }*/
+/* { dg-final { scan-assembler-not "ipop" } }*
 
 /* { dg-final { cleanup-saved-temps } } */
