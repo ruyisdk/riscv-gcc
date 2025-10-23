512c4334aa3e5d6c69f1df5c506dd666ddefc285
Dongyan Chen <chendongyan@isrc.iscas.ac.cn>
2025-10-23 13:02:53 +0800

    RISC-V: Fix 'builtin-rvp32-smmul.c' testcase.

--- gcc/testsuite/gcc.target/riscv/rvp32/builtin-rvp32-smmul.c
+++ gcc/testsuite/gcc.target/riscv/rvp32/builtin-rvp32-smmul.c
@@ -1,6 +1,6 @@
 /* This is a test program for smmul instruction.  */
 /* { dg-do compile { target riscv32*-*-* } } */
-/* { dg-options "-march=rv32gc_zpn -mabi=ilp32d -O3" } */
+/* { dg-options "-march=rv32iafdc_zpn -mabi=ilp32d -O3" } */
 /* { dg-final { check-function-bodies "**" "" "" } } */
 
 #include <rvp_intrinsic.h>
